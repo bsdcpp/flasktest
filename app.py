7ed69a1c806c2ea0bd8e845bd452acf93ce0b569
bsdcpp <bsdcpp@gmail.com>
2025-06-01 12:47:24 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import url_for, Flask, render_template, request
+from flask import url_for, Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 from flask_bootstrap import Bootstrap5
@@ -38,9 +38,22 @@ def login():
         return 'hello'
     else:
         return 'no'
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', name=name)
+    print(request.path, request.args)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 @app.route('/hello')
 def hello():
     return 'Hello'
@@ -48,7 +61,31 @@ def hello():
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
 
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
